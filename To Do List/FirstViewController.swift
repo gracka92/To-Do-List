ebd6c5a873f7f809f2f2713a0e2967b28bbd9829
gracka92 <gracjana.leonowicz@gmail.com>
2017-09-18 13:41:39 +0200

    first commit

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    var items: [String] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
+    {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == .delete
+        {
+            items.remove(at: indexPath.row)
+            table.deleteRows(at: [indexPath], with: .automatic)
+        }
+        UserDefaults.standard.set(items, forKey: "items")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        if let tempItems = itemsObject as? [String] {
+            items = tempItems
+        }
+        table.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
